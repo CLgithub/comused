bc9cbd704bc8bfd7f34c4cc152665f689243b9da
Lmbp <leichen070@gmail.com>
2018-08-07 11:44:21 +0800

    新增http原理图

--- src/main/java/com/cl/comused/FtpComUserd.java
+++ src/main/java/com/cl/comused/FtpComUserd.java
@@ -1,5 +1,6 @@
 package com.cl.comused;
 
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
@@ -17,8 +18,11 @@ public class FtpComUserd {
 
     public static void main(String[] args){
         List<File> files=new ArrayList<File>();
-        files.add(new File("/Users/l/develop/ultrapower/ultrapowerBin/31-/test/"));
-        ftpUpLoadFD(files);
+        files.add(new File("/Users/l/develop/ultrapower/ultrapowerBin/31-/PMS_DATA/"));
+        List<String> fnkeys=new ArrayList<String>();
+        fnkeys.add(".D");
+        fnkeys.add("-all");
+        ftpUpLoadFD(files,fnkeys);
     }
 
     /**
@@ -27,12 +31,12 @@ public class FtpComUserd {
      * @date 2018-7-27
      * @param files
      */
-    public static void ftpUpLoadFD(List<File> files){
+    public static void ftpUpLoadFD(List<File> files,List<String> fnKeys){
         String ftpUrl = "192.168.3.231"; //ftp服务器地址
         int ftpPort = 21; //ftp服务端口
-        String ftpUserName = "username"; //ftp登录名
-        String ftpPassword = "pwd"; //ftp登录密码
-        String ftpPath = "/other/"; //ftp路径
+        String ftpUserName = "ftpuser1"; //ftp登录名
+        String ftpPassword = "123456"; //ftp登录密码
+        String ftpPath = "/other/PMdata/"; //ftp路径
 
         FileInputStream fiStream = null;
         FTPClient ftpClient = null;
@@ -52,7 +56,7 @@ public class FtpComUserd {
                 if(changeWorkingDirectory){
                     ftpClient.setFileTransferMode(ftpClient.BINARY_FILE_TYPE);
                     ftpClient.enterLocalPassiveMode();  //设置本地为被动模式
-                    upDir(files,ftpClient,ftpPath,fiStream);
+                    upDir(files,ftpClient,ftpPath,fiStream, fnKeys);
                 }
             }
 
@@ -82,19 +86,34 @@ public class FtpComUserd {
         }
     }
 
-    public static void upDir(List<File> files, FTPClient ftpClient, String ftpPath, FileInputStream fiStream) throws IOException {
+    public static void upDir(List<File> files, FTPClient ftpClient, String ftpPath, FileInputStream fiStream, List<String> fnKeys) throws IOException {
         int reply;
         for (File file : files) {
             if(file.isDirectory()){
                 boolean b = ftpClient.makeDirectory(ftpPath+file.getName());
                 reply=ftpClient.getReplyCode();     //得到返回参数
                 System.out.println("创建文件夹："+file.getName()+",是否成功："+b+","+reply);
-                upDir(Arrays.asList(file.listFiles()),ftpClient,ftpPath+file.getName()+"/",fiStream);
-            }else if(!".DS_Store".equals(file.getName())){
-                fiStream = new FileInputStream(file);
-                boolean storeFile = ftpClient.storeFile(ftpPath+file.getName(), fiStream);
-                reply=ftpClient.getReplyCode();     //得到返回参数
-                System.out.println("上传"+file.getName()+"是否成功：" + storeFile+","+reply);
+                upDir(Arrays.asList(file.listFiles()),ftpClient,ftpPath+file.getName()+"/",fiStream, fnKeys);
+            }else {
+                if(fnKeys==null||fnKeys.size()==0){
+                    fiStream = new FileInputStream(file);
+                    boolean storeFile = ftpClient.storeFile(ftpPath+file.getName(), fiStream);
+                    reply=ftpClient.getReplyCode();     //得到返回参数
+                    System.out.println("上传"+file.getName()+"是否成功：" + storeFile+","+reply);
+                }else{
+                    boolean b=true;
+                    for(String fnKey:fnKeys){
+                        if(file.getName().contains(fnKey)){
+                            b=false;
+                        }
+                    }
+                    if(b){
+                        fiStream = new FileInputStream(file);
+                        boolean storeFile = ftpClient.storeFile(ftpPath+file.getName(), fiStream);
+                        reply=ftpClient.getReplyCode();     //得到返回参数
+                        System.out.println("上传"+file.getName()+"是否成功：" + storeFile+","+reply);
+                    }
+                }
             }
         }
     }
